068e33405241509aafb66e4ab2f3c9d35aba0ed5
Cosmic AI <support@cosmicjs.com>
2025-07-26 20:49:27 -0700

    Fix deployment error by adding missing primary-200 color to Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
           DEFAULT: '#0ea5e9',
           50: '#f0f9ff',
           100: '#e0f2fe',
+          200: '#bae6fd',
           500: '#0ea5e9',
           600: '#0284c7',
           700: '#0369a1',
